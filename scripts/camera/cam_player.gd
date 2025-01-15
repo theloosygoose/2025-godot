49a85708cd419e09e8bfd6fae126a8862f082873
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2025-01-15 17:54:03 -0500

    finished cleaning up the code for camera2d

--- scripts/camera/cam_player.gd
+++ scripts/camera/cam_player.gd
@@ -2,8 +2,4 @@ extends Camera2D
 class_name CameraPlayer 
 
 # Camera control settings
-@export var pan_speed: float = 500.0         # Speed for manual camera movement
-@export var movement_smoothing: float = 10.0  # Higher value = smoother movement
-@export var stick_deadzone: float = 0.2      
-
 @export var player_scene_node: PlayerSceneNode
